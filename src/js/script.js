94d08a81c29ff81c40aaac01d1d90779fbe63b8a
Matheus <theusantunesm@gmail.com>
2025-03-31 09:08:04 -0300

    declarações e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// declarações de variaveis
+
+var nome = "fiap";
+console.log(nome);
+
+let idade =19;
+console.log(idade)
+
+const sobrenome="fiapinho"
+console.log(sobrenome);
+
+
+// declarando uma variavel indefinida
+let nome1;
+console.log(nome1)
+
+// declarando uma variavel nula
+let nome2=null;
+console.log(nome2)
