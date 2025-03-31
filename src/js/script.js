b91a3870c5dbad10fc8c777a394d41c82f12ac22
Matheus <theusantunesm@gmail.com>
2025-03-31 09:13:50 -0300

    tipos de variaveis -typeof

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,19 @@ console.log(nome1)
 // declarando uma variavel nula
 let nome2=null;
 console.log(nome2)
+
+// tipos de variaveis
+let exemplo1 =10;
+console.log(typeof exemplo1)
+
+let exemplo2 ="tecnologia";
+console.log(typeof exemplo2)
+
+let exemplo3 =true;
+console.log(typeof exemplo3);
+
+let exemplo4={};
+console.log(typeof exemplo4)
+
+let exemplo5 =["aluno1","aluno2"];
+console.log(typeof exemplo5)
